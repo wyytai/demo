1e3728a1a2b5debbb37b36c79ec91753dd691a03
seekmac002 <wyytai@qq.com>
2017-04-21 16:27:06 +0800

    -[FIX]log

--- 123/123/AppDelegate.m
+++ 123/123/AppDelegate.m
@@ -16,6 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    NSLog(@"wwww");
     // Override point for customization after application launch.
     return YES;
 }
